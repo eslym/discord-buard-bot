8f4d0e80427b3ff6772038b8bf53059f895f9c6a
0nepeop1e <engshun.lim@gmail.com>
2021-06-20 16:16:25 +0800

    v2

---
+++ fonts/fonts.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    "Dancing Script": path.resolve(__dirname, 'DancingScript-Regular.ttf'),
+    "Limelight": path.resolve(__dirname, 'Limelight-Regular.ttf'),
+    "Long Cang": path.resolve(__dirname, 'LongCang-Regular.ttf'),
+    "Suez One": path.resolve(__dirname, 'SuezOne-Regular.ttf'),
+    "ZCOOL KuaiLe": path.resolve(__dirname, 'ZCOOLKuaiLe-Regular.ttf'),
+}
--- index.js
+++ index.js
@@ -1,153 +1,174 @@
+/**
+ * @typedef {{message: dc.Message, clicker: {user:dc.User, member: dc.GuildMember}, channel: dc.Channel, id: string}} ButtonEvent
+ */
+
+const {MessageActionRow, MessageButton} = require("discord-buttons");
+
 const process = require('process');
 const fs = require('fs');
-const dotenv = require('dotenv');
-const Captcha = require('@haileybot/captcha-generator');
+const logger = require('./lib/logger');
+
+const createCaptchaCanvas = require('./lib/captcha');
 
 const dc = require('discord.js');
 const bot = new dc.Client();
 
-let throttle_count = {};
-let throttle_expires = {};
-let throttle_ban = {};
-let captcha_answer = {};
-let captcha_expires = {};
+const dcBtn = require('discord-buttons');
+
+dcBtn(bot);
 
 if (process.argv.includes('init')){
-    if (!fs.existsSync('.env'))
-        fs.copyFileSync('.env.example', '.env');
-    if (!fs.existsSync('greetings.txt'))
-        fs.copyFileSync('greetings.txt.example', 'greetings.txt');
-    if (!fs.existsSync('throttle.txt'))
-        fs.copyFileSync('throttle.txt.example', 'throttle.txt');
-    if (!fs.existsSync('joined.txt'))
-        fs.copyFileSync('joined.txt.example', 'joined.txt');
-    console.log("Initial files generated.");
+    if (!fs.existsSync('config.yml'))
+        fs.copyFileSync('config.yml.example', 'config.yml');
+    logger.log("Initial files generated.");
     process.exit();
 }
 
-const greetings_message = fs.readFileSync('greetings.txt')
-    .toString('utf8');
-const throttle_message = fs.readFileSync('throttle.txt')
-    .toString('utf8');
-const joined_message = fs.readFileSync('joined.txt')
-    .toString('utf8');
-
-dotenv.config();
+const config = require('./lib/config');
+const limiter = require('./lib/limiter');
 
 bot.on('ready', function (){
-    console.log(`Bot ready, logged in as ${bot.user.tag}!`);
+    logger.log(`Bot ready, logged in as ${bot.user.tag}(${bot.user.id}).`);
 });
 
-const message_handler = async function (message){
+/**
+ * @param {dc.TextChannel} channel
+ * @param {dc.User} user
+ */
+async function sendCaptcha(channel, user){
+    let captcha = createCaptchaCanvas();
+    await limiter.setAnswer(user.id, captcha.answer);
+    let msg = new dc.MessageEmbed();
+    msg.setImage('attachment://captcha.jpg');
+    msg.attachFiles([new dc.MessageAttachment(captcha.canvas.createJPEGStream(), 'captcha.jpg')]);
+    let choices = Array(4).fill(0).map(()=>createCaptchaCanvas.createPin());
+    choices.push(captcha.answer);
+    choices.sort(()=>Math.random() - 0.5);
+    let row = new MessageActionRow();
+    for (let choice of choices) {
+        let btn = new MessageButton();
+        btn.setID('server_guard_captcha_ans_'+choice);
+        btn.setStyle('green');
+        btn.setLabel(choice);
+        row.addComponent(btn);
+    }
+    let reset = new MessageButton();
+    reset.setID('server_guard_captcha_reload');
+    reset.setStyle('grey');
+    reset.setLabel(config('lang.button.reset'));
+    let row2 = new MessageActionRow();
+    row2.addComponent(reset);
+    logger.log(`${user.tag}(${user.id}) requested new captcha.`);
+    return channel.send(config('lang.message.send'), {embed: msg, components: [row, row2] });
+}
+
+/** @param {dc.Message} message */
+const message_handler = async function ( message){
     if(message.channel.type !== 'dm' || message.author.bot){
         return;
     }
-    let front_guild = bot.guilds.resolve(process.env.FRONT_GUILD_ID);
-    let protected_guild = bot.guilds.resolve(process.env.GUARDED_GUILD_ID);
+    let front_guild = bot.guilds.resolve(config('bot.server.shadow'));
+    let member = await front_guild.members.fetch(message.author.id).catch(()=>null);
+    if(member === null){
+        // Ignore when the author is not in the shadow server.
+        return;
+    }
+    let protected_guild = bot.guilds.resolve(config('bot.server.guarded'));
     let usr = await protected_guild.members.fetch(message.author.id).catch(()=>null);
     if(usr !== null){
-        await message.channel.send(joined_message);
-        let member = await front_guild.members.fetch(message.author.id);
-        if(member === null){
-            console.log(`${message.author.tag} not exists in shadow server, ignored.`);
-        }
         if(!member.hasPermission('ADMINISTRATOR')) {
-            console.log(`${message.author.tag} already in the destination server, kicked.`);
+            logger.log(`${message.author.tag}(${message.author.id}) already in the destination server, kicked.`);
             return member.kick('Already in destination server.');
         } else {
-            console.log(`${message.author.tag} already in the destination server, ignored because of admin permission.`);
+            logger.log(`${message.author.tag}(${message.author.id}) already in the destination server, ignored because of admin permission.`);
         }
         return;
     }
-    if(throttle_ban.hasOwnProperty(message.author.id)){
-        return message.channel.send(throttle_message);
+    if(await limiter.isBan(message.author.id)){
+        return message.channel.send(config('lang.message.throttle'));
     }
-    if(!throttle_count.hasOwnProperty(message.author.id)){
-        throttle_count[message.author.id] = 0;
-    } else {
-        throttle_count[message.author.id] += 1;
-        if(!throttle_expires.hasOwnProperty(message.author.id)){
-            throttle_expires[message.author.id] = setTimeout(function (){
-                delete throttle_count[message.author.id];
-                delete throttle_expires[message.author.id];
-            }, 300000);
-        }
-        if(throttle_count[message.author.id] > process.env.THROTTLE_FIVE_MIN){
-            console.log(`${message.author.tag} reached throttle limit.`);
-            throttle_ban[message.author.id] = setTimeout(function (){
-                delete throttle_ban[message.author.id];
-            }, process.env.THROTTLE_BAN_SECONDS * 1000);
-            return message.channel.send(throttle_message);
-        }
+    if(await limiter.isBan(message.author.id)){
+        return message.channel.send(config('lang.message.throttle'));
     }
-    if(captcha_expires.hasOwnProperty(message.author.id)){
-        clearTimeout(captcha_expires[message.author.id]);
-        delete captcha_expires[message.author.id];
-    }
-    if(
-        captcha_answer.hasOwnProperty(message.author.id) &&
-        captcha_answer[message.author.id] === message.content.toUpperCase()
-    ){
-        let ch = protected_guild.channels.resolve(protected_guild.systemChannelID);
-        let invite = await ch.createInvite({
-            maxAge: 3600,
-            maxUses: 1,
-            unique: true,
-            reason: 'Captcha verified',
-        });
-        console.log(`${message.author.tag} passed the captcha, attempts: ${throttle_count[message.author.id]}`);
-        delete throttle_count[message.author.id];
-        if(throttle_ban.hasOwnProperty(message.author.id)){
-            clearTimeout(throttle_ban[message.author.id]);
-            delete throttle_ban[message.author.id];
-        }
-        delete captcha_answer[message.author.id];
-        delete captcha_expires[message.author.id];
-        await message.channel.send(invite.url);
-        if(throttle_count[message.author.id] >= process.env.THROTTLE_FIVE_MIN){
-            message.channel.send(throttle_message);
-        }
-        return;
-    }
-    await message.channel.send(greetings_message);
-    let captcha = new Captcha(4);
-    captcha_answer[message.author.id] = captcha.value;
-    captcha_expires[message.author.id] = setTimeout(function (){
-        delete captcha_answer[message.author.id];
-    }, 30000);
-    let attach = new dc.MessageAttachment(captcha.JPEGStream, 'captcha.jpg');
-    console.log(`${message.author.tag} requested new captcha, attempts: ${throttle_count[message.author.id]}`);
-    return message.channel.send(attach);
+    return sendCaptcha(message.channel, message.author);
 };
 
 bot.on('message', msg => message_handler(msg).catch(function(err){
-    console.error(err);
+    logger.error(err);
     let message = new dc.MessageEmbed();
     message.setTitle('Sorry, something went wrong :(');
     message.setColor('RED');
     msg.channel.send(message)
-        .catch(console.error);
+        .catch(logger.error);
+}));
+
+/** @param {ButtonEvent} btn */
+let button_handler = async (btn)=>{
+    if(btn.id.startsWith('server_guard_captcha')){
+        await btn.defer();
+        await btn.message.delete();
+        if(btn.id === 'server_guard_captcha_reload'){
+            return sendCaptcha(btn.channel, btn.clicker.user);
+        }
+        let ans = btn.id.replace('server_guard_captcha_ans_', '');
+        let attempts = await limiter.incrAttempts(btn.clicker.user.id);
+        if(ans === await limiter.getAnswer(btn.clicker.user.id)){
+            await limiter.clearAttempts(btn.clicker.user.id);
+            logger.log(`${btn.clicker.user.tag}(${btn.clicker.user.id}) passed captcha, attempts: ${attempts}.`);
+            let protected_guild = await bot.guilds.fetch(config('bot.server.guarded'));
+            let ch = protected_guild.channels.resolve(protected_guild.systemChannelID);
+            let invite = await ch.createInvite({
+                maxAge: 3600,
+                maxUses: 1,
+                unique: true,
+                reason: 'Captcha verified',
+            });
+            let join = new MessageButton();
+            join.setLabel(config('lang.button.join'));
+            join.setStyle('url');
+            join.setURL(invite.url);
+            return btn.channel.send(config('lang.message.success'), {component: join});
+        } else {
+            if(attempts >= config('bot.throttle.limit')){
+                await limiter.ban(btn.clicker.user.id);
+                logger.log(`${btn.clicker.user.tag}(${btn.clicker.user.id}) banned.`);
+                return btn.channel.send(config('lang.message.throttle'));
+            } else {
+                logger.log(`${btn.clicker.user.tag}(${btn.clicker.user.id}) failed captcha, attempts: ${attempts}.`);
+                return btn.channel.send(config('lang.message.failed'));
+            }
+        }
+    }
+};
+
+bot.on('clickButton', btn => button_handler(btn).catch(function(err){
+    logger.error(err);
+    let message = new dc.MessageEmbed();
+    message.setTitle('Sorry, something went wrong :(');
+    message.setColor('RED');
+    btn.channel.send(message)
+        .catch(logger.error);
 }));
 
 bot.on('guildMemberAdd', (m) => (async function (member) {
-    if(member.guild.id === process.env.GUARDED_GUILD_ID){
-        console.log(`${member.user.tag} joined destination server.`);
-        let front_guild = bot.guilds.resolve(process.env.FRONT_GUILD_ID);
+    if(member.guild.id === config('bot.server.guarded')){
+        logger.log(`${member.user.tag}(${member.user.id}) joined destination server.`);
+        let front_guild = bot.guilds.resolve(config('bot.server.shadow'));
         let mem = await front_guild.members.fetch(member.id).catch(()=>null);
         if(mem === null){
-            console.log(`${member.user.tag} not exists in shadow server, ignored.`);
+            logger.log(`${member.user.tag}(${member.user.id}) not exists in shadow server, ignored.`);
         }
         if(mem.hasPermission('ADMINISTRATOR')) {
-            console.log(`${member.user.tag} not kicked from shadow server because of admin permission.`);
+            logger.log(`${member.user.tag}(${member.user.id}) not kicked from shadow server because of admin permission.`);
         } else {
-            console.log(`${mem.user.tag} kicked from shadow server.`);
+            logger.log(`${mem.user.tag}(${member.user.id}) kicked from shadow server.`);
             return mem.kick('Already in destination server.');
         }
     }
-})(m).catch(console.error));
+})(m).catch(logger.error));
 
-bot.login(process.env.DISCORD_BOT_TOKEN)
+bot.login(config('bot.token'))
     .catch(function (error){
-        console.error(error);
+        logger.error(error);
         process.exit(1);
     });
